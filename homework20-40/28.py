5fc916e8152f93193531ee6da7fd21438f967f86
meguru <1401080341@qq.com>
2022-10-18 15:44:19 +0800

    10.18 15:44

--- homework20-40/28.py
+++ homework20-40/28.py
@@ -1,4 +1,5 @@
 import random
+from this import d
 a=random.randint(1,50)
 list1=random.sample([i for i in range (100)],a)
 print (list1)
@@ -12,5 +13,11 @@ for i in list1:
             list2.append(i)
         else:
             continue
-
 print (list2)
+res=0
+for x in list2:
+    for y in list2:
+        p=x+y
+        if p in list2:
+            res+=1
+print (format(res/2,'.0f'))
